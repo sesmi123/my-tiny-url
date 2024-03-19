3e68562458f22a407239a415d4561ec8d3da3989
Sesmi Athiyarath <sesmi.athiyarath@siemens-healthineers.com>
2024-03-20 01:35:20 +0530

    create method for configuring logger

--- my-tiny-url/my_tiny_url/app.py
+++ my-tiny-url/my_tiny_url/app.py
@@ -4,13 +4,19 @@ from flask import Flask, abort, jsonify, redirect, request
 import validators
 from tiny_url import TinyURL
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
+def configure_logging():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    return logger
+
+logger = configure_logging()
 
 app = Flask(__name__)
 tiny_url = TinyURL()
