613cf46ca3f40902c40b7b5b8e8dd697877c7663
Sesmi Athiyarath <sesmi.athiyarath@siemens-healthineers.com>
2024-03-20 01:43:20 +0530

    refactor code

--- my-tiny-url/my_tiny_url/app.py
+++ my-tiny-url/my_tiny_url/app.py
@@ -1,8 +1,8 @@
 import logging
 import sys
-from flask import Flask, abort, jsonify, redirect, request
-import validators
+from flask import Flask, request
 from tiny_url import TinyURL
+from tiny_url_controller import TinyURLController
 
 def configure_logging():
     logger = logging.getLogger(__name__)
@@ -20,6 +20,7 @@ logger = configure_logging()
 
 app = Flask(__name__)
 tiny_url = TinyURL()
+controller = TinyURLController(tiny_url, logger)
 
 @app.route('/hello')
 def hello():
@@ -27,39 +28,11 @@ def hello():
 
 @app.route('/create-url', methods=['POST'])
 def create_short_url():
-    if not request.is_json or 'url' not in request.get_json():
-        abort(400, description="Request must be JSON and contain 'url' key.")
-
-    long_url = request.get_json()['url']
-
-    if not validators.url(long_url):
-        abort(400, description="The provided 'url' is not valid.")
-
-    short_url = tiny_url.get_short_url(long_url)
-
-    if short_url:
-        response = {
-            "original": long_url,
-            "short_url": short_url
-        }
-        logger.debug(response)
-        return jsonify(response), 200
-
-    short_url = tiny_url.shorten_url(long_url)
-    response = {
-        "original": long_url,
-        "short_url": short_url
-    }
-    return jsonify(response), 201
+    return controller.create_short_url(request)
 
 @app.route('/<short_url>')
 def get_long_url(short_url):
-    long_url = tiny_url.get_long_url(short_url)
-    if not long_url:
-        logger.error(f"No URL found for {request.url}")
-        return "No URL found!",500
-    logger.info(f"Redirecting to {long_url}")
-    return redirect(long_url, code=301)
+    return controller.get_long_url(request, short_url)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
---
+++ my-tiny-url/my_tiny_url/tiny_url_controller.py
@@ -0,0 +1,41 @@
+from flask import jsonify, abort, redirect
+import validators
+
+class TinyURLController:
+    def __init__(self, tiny_url, logger):
+        self.logger = logger
+        self.tiny_url = tiny_url
+
+    def create_short_url(self, request):
+        if not request.is_json or 'url' not in request.get_json():
+            abort(400, description="Request must be JSON and contain 'url' key.")
+
+        long_url = request.get_json()['url']
+
+        if not validators.url(long_url):
+            abort(400, description="The provided 'url' is not valid.")
+
+        short_url = self.tiny_url.get_short_url(long_url)
+
+        if short_url:
+            response = {
+                "original": long_url,
+                "short_url": short_url
+            }
+            self.logger.debug(response)
+            return jsonify(response), 200
+
+        short_url = self.tiny_url.shorten_url(long_url)
+        response = {
+            "original": long_url,
+            "short_url": short_url
+        }
+        return jsonify(response), 201
+
+    def get_long_url(self, request, short_url):
+        long_url = self.tiny_url.get_long_url(short_url)
+        if not long_url:
+            self.logger.error(f"No URL found for {request.url}")
+            return "No URL found!",500
+        self.logger.info(f"Redirecting to {long_url}")
+        return redirect(long_url, code=301)
